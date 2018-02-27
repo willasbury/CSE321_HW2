6215087299eef02fee0d8a22153bacbab9d37bc0
will <will@asburywg>
2018-02-27 12:17:48 -0500

    first commit

---
+++ src/CalcTestCases.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class CalcTestCases {
+	
+	@Test
+	public void testSum() {
+		int result = Calc.add(2, 2);
+		assertTrue(result == 4);
+	}
+
+	@Test
+	public void testDifference() {
+		int result = Calc.subtract(4, 2);
+		assertTrue(result == 2);
+	}
+	
+	@Test
+	public void testMultiply() {
+		int result = Calc.multiply(5, 2);
+		assertTrue(result == 10);
+	}
+	
+	@Test
+	public void testDivision() {
+		int result = Calc.divide(10, 5);
+		assertTrue(result == 2);
+	}
+
+}
